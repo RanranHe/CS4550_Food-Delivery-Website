a5cf5386875f2a1e65d8f1ca04522a69e32d7770
Xingyao Wu <gurge3@gmail.com>
2017-06-13 21:49:28 -0400

    Configure MLab MongoDB address

--- js/app.js
+++ js/app.js
@@ -1,7 +1,21 @@
 module.exports = function(app) {
     var mongoose = require('mongoose');
     mongoose.Promise = require('q').Promise;
-    mongoose.connect('mongodb://127.0.0.1/RollingFood');
+
+    // Used temporarily for accessing MLab MongoDB.
+    var username = "admin";
+    var password = "admin";
+    var connectionString = 'mongodb://'+ username + ':' + password + '@ds019756.mlab.com:19756/heroku_flv5vpkg';
+
+    if(process.env.MLAB_USERNAME) {
+        connectionString = process.env.MLAB_USERNAME + ":" +
+            process.env.MLAB_PASSWORD + "@" +
+            process.env.MLAB_HOST + ':' +
+            process.env.MLAB_PORT + '/' +
+            process.env.MLAB_APP_NAME;
+    }
+    mongoose.connect(connectionString);
+
 
     require('./services/UserService.service.server.js')(app);
 };
