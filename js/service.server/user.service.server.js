82dcc85f6e4a36f882124c7e5465c511a82ee399
cangxuexiao <cangxuexiao@gmail.com>
2017-06-19 19:32:37 +0800

    Login

--- js/service.server/user.service.server.js
+++ js/service.server/user.service.server.js
@@ -1,8 +1,37 @@
 module.exports = function (app, models) {
     var userModel = models.userModel;
 
-    app.post('/api/user', createUser);
+    // Register
+    var bcrypt = require("bcrypt-nodejs");
+
+    // Login
+    var passport = require('passport');
+    var LocalStrategy = require('passport-local').Strategy;
+    passport.use(new LocalStrategy(localStrategy));
+    passport.serializeUser(serializeUser);
+    passport.deserializeUser(deserializeUser);
+
+
+    app.post('/api/project/register', register);
+    app.post('/api/project/user', createUser);
     app.get('/api/project/user/', findUserByUsername);
+
+    app.post("/api/project/login", passport.authenticate('local'), login);
+    app.get('/api/project/checkLoggedIn', checkLoggedIn);
+
+    //////////////// Register ////////////////
+    function register(req, res) {
+        var user = req.body;
+        console.log("register: " + user.username + "  " + user.password)
+        user.password = bcrypt.hashSync(user.password);
+        userModel
+            .createUser(user)
+            .then(function (user) {
+                req.login(user, function (status) {
+                    res.json(user);
+                });
+            });
+    }
 
     function findUserByUsername(req, res) {
         var username = req.query['username'];
@@ -23,5 +52,53 @@ module.exports = function (app, models) {
             .then(function (user) {
                 res.json(user);
             });
+    }
+
+    /////////////// Login ///////////////
+    function localStrategy(username, password, done) {
+        userModel
+            .findUserByUsername(username)
+            .then(function (user) {
+                if (user && bcrypt.compareSync(password, user.password)) {
+                    done(null, user);
+                } else {
+                    done(null, false);
+                }
+            }, function (error) {
+                done(error, false);
+            })
+    }
+
+    function login(req, res) {
+        var user = req.user;
+        res.json(user);
+    }
+
+    function checkLoggedIn(req, res) {
+        console.log(req.user);
+        if (req.isAuthenticated()) {
+            console.log("if")
+            res.json(req.user);
+        } else {
+            console.log("else")
+            res.send('0');
+        }
+    }
+
+    function serializeUser(user, done) {
+        done(null, user);
+    }
+
+    function deserializeUser(user, done) {
+        userModel
+            .findUserById(user._id)
+            .then(
+                function (user) {
+                    done(null, user);
+                },
+                function (err) {
+                    done(err, null);
+                }
+            );
     }
 };
